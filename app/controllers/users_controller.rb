0e5e0409e4f68257afd0c015f0846d988f8fbb13
Chris Nguyen <chrisngn3@gmail.com>
2016-03-30 11:14:42 -0400

    Add the ability to create user and disable creating expenses when no users are present

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,40 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		init_user @user
+
+		if @user.save
+			flash[:success] = "User added!"
+			redirect_to '/users'
+		else
+			render 'new'
+		end
+	end
+
 	private
-	def print_hash hash
-		s = ""
-		hash
-		return s
+	def user_params
+		params.require(:user).permit(:name, :amount_paid, :owe)
+	end
+
+	def init_user user
+		# set hash
+		User.all.each do |u|
+			# set for the new user
+			user.owe[u.name] = 0.0
+
+			# add the new user to the old users
+			u.owe[user.name] = 0.0 unless u.owe.key?(user.name)
+			u.save
+		end
+
+		# set amount_paid
+		user.amount_paid = 0.0
 	end
 
 end # UsersController
